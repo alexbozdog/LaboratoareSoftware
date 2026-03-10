517a262c978d157b43ab482bfcf3bef9a80f4066
alexbozdog <alexandrustefan.bozdog@ulbsibiu.ro>
2026-03-10 08:53:48 +0200

    lab2

--- src/main/java/ro/ulbs/proiectaresoftware/lab1/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/lab1/Application.java
@@ -1,4 +1,38 @@
 package ro.ulbs.proiectaresoftware.lab1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (char i = 'a'; i <= 'z'; i++) {
+            sb.append(i);
+        }
+        String alfabet = sb.toString();
+
+        System.out.println("Litere mici: " + alfabet.toLowerCase());
+        System.out.println("Majuscule: " + alfabet.toUpperCase());
+
+
+        String vocale = "aeiou";
+        List<String> segmente = new ArrayList<>();
+
+        int start = 0;
+        for (int i = 1; i < alfabet.length(); i++) {
+            char caracterCurent = alfabet.charAt(i);
+
+            if (vocale.indexOf(caracterCurent) != -1) {
+                segmente.add(alfabet.substring(start, i));
+                start = i;
+            }
+        }
+        segmente.add(alfabet.substring(start));
+
+        String[] arrayRezultat = segmente.toArray(new String[0]);
+
+        System.out.println("Array-ul rezultat:");
+        for (int i = 0; i < arrayRezultat.length; i++) {
+            System.out.println("array[" + i + "] = \"" + arrayRezultat[i] + "\";");
+        }
+    }
 }
---
+++ src/main/java/ro/ulbs/proiectaresoftware/lab2/Main.java
@@ -0,0 +1,79 @@
+package ro.ulbs.proiectaresoftware.lab2;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> x = new ArrayList();
+        List<Integer> y = new ArrayList();
+        List<Integer> xPlusY = new ArrayList(); //a
+        Set<Integer> zSet = new TreeSet(); //b
+        List<Integer> xMinusY = new ArrayList();//c
+        int p = 4;
+        List<Integer> xPlusYLimitedByP = new ArrayList(); //d
+
+
+        Random rand = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            x.add(rand.nextInt(11));
+        }
+
+        for (int i = 0; i < 7; i++) {
+            y.add(rand.nextInt(11));
+        }
+
+        Collections.sort(x);
+        Collections.sort(y);
+
+        // a)reuniunea
+        xPlusY.addAll(x);
+        xPlusY.addAll(y);
+        Collections.sort(xPlusY);
+
+        // b) intersectia
+        for (Integer val : x) {
+            if (y.contains(val)) {
+                zSet.add(val);
+            }
+        }
+
+        // c) x - y
+        for (Integer val : x) {
+            if (!y.contains(val)) {
+                xMinusY.add(val);
+            }
+        }
+
+        // d) valori <= p
+        for (Integer val : x) {
+            if (val <= p) {
+                xPlusYLimitedByP.add(val);
+            }
+        }
+
+        for (Integer val : y) {
+            if (val <= p) {
+                xPlusYLimitedByP.add(val);
+            }
+        }
+
+        Collections.sort(xPlusYLimitedByP);
+
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("xPlusY = " + xPlusY);
+        System.out.println("zSet = " + zSet);
+        System.out.println("xMinusY = " + xMinusY);
+        System.out.println("xPlusYLimitedByP = " + xPlusYLimitedByP);
+
+
+
+
+
+
+
+
+    }
+}
